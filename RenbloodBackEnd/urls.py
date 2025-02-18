7c439724e7c1b1d795b99f8389caf3c50fcff61d
Alban MORAGNY <alban.yolxe@gmail.com>
2025-02-18 21:27:59 +0100

    Version adapté pour le 1er test en Prod corrigé 9

--- RenbloodBackEnd/urls.py
+++ RenbloodBackEnd/urls.py
@@ -1,8 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Bienvenue sur Renblood API 🎉"})
+
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path('players/', include('players.urls')),
 ]
