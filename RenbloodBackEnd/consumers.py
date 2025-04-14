78444eec340e25456f96360ef9757f7478d011d0
MORAGNY Alban <alban.moragny@montpellier.fr>
2025-04-14 11:55:54 +0200

    Modif Consumers

--- RenbloodBackEnd/consumers.py
+++ RenbloodBackEnd/consumers.py
@@ -1,16 +1,19 @@
 import json
+import random
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class DiceConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.channel_layer.group_add("dice_room", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard("dice_room", self.channel_name)
+        pass
 
-    async def dice_rolled(self, event):
-        await self.send(text_data=json.dumps({
-            'type': 'dice_result',
-            'value': event['value']
-        }))
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        if data.get("type") == "roll":
+            result = random.randint(1, 20)
+            await self.send(text_data=json.dumps({
+                "type": "dice_result",
+                "value": result
+            }))
