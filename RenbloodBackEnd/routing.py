8123d19d41de9b04d2e255b779d041aafdd4c91e
MORAGNY Alban <alban.moragny@montpellier.fr>
2025-04-14 11:11:04 +0200

    Modif routing

--- RenbloodBackEnd/routing.py
+++ RenbloodBackEnd/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from consumers import DiceConsumer
+from .consumers import DiceConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/dice/$', DiceConsumer.as_asgi()),
