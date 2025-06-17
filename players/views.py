a7c56346b2def84107e793fd80afcc8804275e66
Alban MORAGNY <alban.yolxe@gmail.com>
2025-06-17 22:03:14 +0200

    Petite modif

--- players/views.py
+++ players/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+    from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Player
